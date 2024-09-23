e9571416064bee2d43caed88c115c2225cbdc211
sunlei246 <sunlei246@crland.com.cn>
2024-09-23 21:06:14 +0800

    select sort

--- select-sort.py
+++ select-sort.py
@@ -1,1 +1,24 @@
 
+from typing import List
+
+
+def select_sort(list: List[int]) -> List[int]:
+    length = len(list)
+
+    for i in range(0, length - 1):
+        # min value index
+        index = i
+
+        for j in range(i + 1, length):
+            if list[j] < list[index]:
+                index = j
+
+        if index != i:
+            list[index], list[i] = list[i], list[index]
+
+    return list
+
+
+print(select_sort([1, 101, -3, 4, 100, 8]))
+print(select_sort([1]))
+print(select_sort([]))
