a0c45cd8fd84d0a1c208e088edcc26edbd9d92f9
sunlei246 <sunlei246@crland.com.cn>
2024-09-23 18:44:33 +0800

    insert sort

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # algorithms-in-python
 Python algorithm solutions for personal practice
+
+1. To maintain logic clarity, invalid input processing has been ignored
+2. Marginal condition should always be considered correctly!
---
+++ insert-sort.py
@@ -0,0 +1,41 @@
+def insert_sort(list):
+    length = len(list)
+
+    for pos in range(1, length):
+        # pick one elment to insert
+        val = list[pos]
+        # current index
+        index = pos
+
+        # is left element bigger than val
+        while list[index - 1] > val and index > 0:
+            # move to right
+            list[index] = list[index - 1]
+            index -= 1
+
+        list[index] = val
+
+    return list
+
+def insert_sort2(list):
+    length = len(list)
+
+    for pos in range(1, length):
+        # pick one elment to insert
+        val = list[pos]
+        # left index
+        index = pos - 1
+
+        # is left element bigger than val
+        while list[index] > val and index > 0:
+            # move to right
+            list[index + 1] = list[index]
+            index -= 1
+
+        # current
+        list[index + 1] = val
+
+    return list
+
+print(insert_sort([1, 101, -3, 4, 100, 8]))
+print(insert_sort2([1, 101, -3, 4, 100, 8]))
