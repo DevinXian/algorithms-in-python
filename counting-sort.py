3f83b5ab7e280141375390b7ed18c7a59fae6cd9
sunlei246 <sunlei246@crland.com.cn>
2024-09-24 15:54:27 +0800

    counting sort

--- counting-sort.py
+++ counting-sort.py
@@ -5,7 +5,7 @@ def countingSort(a: list) -> list:
 
     counter: list = [0] * valueRange
 
-    # count for element 
+    # count for element
     for item in a:
         counter[item - minVal] += 1
 
@@ -19,32 +19,48 @@ def countingSort(a: list) -> list:
 
     # loop a, better then loop counter, but prefix sum required
 
-    # unstable
+    # to right, unstable
     for value in a:
 
-    # # reverse a -> stable
-    # for index in range(len(a) - 1, -1, -1):
-    #     value = a[index]
+        # to left, stable
+        # for index in range(len(a) - 1, -1, -1):
+        #     value = a[index]
         # find index, place the element
         result[counter[value - minVal] - 1] = value
         # prefix sum - 1
         counter[value - minVal] -= 1
 
-    # version 2
-    # result: list = [None] * len(a)
-    # i = 0
+    return result
 
-    # loop counter ## loop a is better
-    # for val, count in enumerate(counter):
-    #   cnt = count
 
-    #   while cnt > 0:
-    #     result[i] = val - minVal
-    #     i += 1
-    #     cnt -= 1
+def countingSort2(a: list) -> list:
+    minVal: int = min(a)
+    valueRange: int = max(a) - minVal + 1
+
+    counter: list = [0] * valueRange
+
+    # count for element
+    for item in a:
+        # absolute value
+        counter[item - minVal] += 1
+
+    result: list = [None] * len(a)
+    i = 0
+
+    # loop counter - no need for prefix sum
+    for val, count in enumerate(counter):
+        # restore val by + minVal
+        cnt = count
+
+        while cnt > 0:
+            result[i] = val + minVal
+            i += 1
+            cnt -= 1
 
     return result
 
 
 print(countingSort([652, 721, 177, 977, 24, 17, 126, 515, 442, 917]))
-print(countingSort([5, 3, 5, 4, 10, 3])) 
+print(countingSort2([652, 721, 177, 977, 24, 17, 126, 515, 442, 917]))
+print(countingSort([5, 3, 5, 4, 10, 3]))
+print(countingSort2([5, 3, 5, 4, 10, 3]))
