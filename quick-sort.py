d7c21dad93da76926b62ed771998bf0d529243bf
sunlei246 <sunlei246@crland.com.cn>
2024-09-24 11:41:01 +0800

    another partition method for quick-sort

--- quick-sort.py
+++ quick-sort.py
@@ -2,29 +2,48 @@ from random import shuffle
 from typing import List
 
 
+# def partition(list: List, start, end):
+#     i = start
+#     j = end
+#     pivotVal = list[start]
+
+#     while i < j:
+#         while list[i] <= pivotVal and i < end:
+#             i += 1
+
+#         # until element <= pivotVal => pivot should be swaped here
+#         while list[j] > pivotVal and j > start:
+#             j -= 1
+
+#         # no overlap, continue
+#         if i < j:
+#             list[i], list[j] = list[j], list[i]
+#         else:
+#             break
+
+#     # swap pivot to target index (list[j] is the last <= pivotVal element)
+#     list[start], list[j] = list[j], list[start]
+
+#     return j
+
+# partition2
+
 def partition(list: List, start, end):
     i = start
-    j = end
-    pivotVal = list[start]
+    pivot = list[start]
 
-    while i < j:
-        while list[i] <= pivotVal and i < end:
+    # check element
+    for j in range(start + 1, end + 1):
+        # little than pivot, move to i + 1
+        if list[j] < pivot:
+            # move right, receive next element <= pivot
             i += 1
-
-        # until element <= pivotVal => pivot should be swaped here
-        while list[j] > pivotVal and j > start:
-            j -= 1
-
-        # no overlap, continue
-        if i < j:
+            # swap
             list[i], list[j] = list[j], list[i]
-        else:
-            break
 
-    # swap pivot to target index (list[j] is the last <= pivotVal element)
-    list[start], list[j] = list[j], list[start]
+    list[start], list[i] = list[i], list[start]
 
-    return j
+    return i
 
 
 def quickSort(list: List[int], start, end):
@@ -44,12 +63,11 @@ def getShuffledList(arr: List) -> List:
     return newArr
 
 
-arr = [-10, 1, 2, 5, 11, 10, 9, 10, 100, 3]
+arr = [8, 1, 2, -5, 11, 10, 9, 10, 100, 3]
+
+partition(arr, 0, 9)
 
 for i in range(10):
     randomArr = getShuffledList(arr)
     quickSort(randomArr, 0, len(arr) - 1)
     print(randomArr)
-
-
-
